cc8b6e5625695fa7416f364cf0e61a718057c736
Serhii Staryk <serhii.staryk@gmail.com>
2024-02-24 15:27:18 +0200

    fix drag behaviour

--- src/App.js
+++ src/App.js
@@ -1,37 +1,41 @@
 import React, { useRef, useState } from 'react';
 
+import ComponentToPrint from './components/ComponentToPrint/ComponentToPrint';
+import ControlPanel from './components/ControlPanel/ControlPanel';
+import DraftEditor from './components/DraftEditor/DraftEditor';
 import Footer from './components/Footer/Footer';
 import Modal from './components/UI/Modal/Modal';
 import Sidebar from './components/Sidebar/Sidebar';
-import DraftEditor from './components/DraftEditor/DraftEditor';
-import ControlPanel from './components/ControlPanel/ControlPanel';
-import ComponentToPrint from './components/ComponentToPrint/ComponentToPrint';
 
 const App = () => {
   const [showModal, setShowModal] = useState(false);
+
   const targetRef = useRef();
 
-  const handleShowModal = () => {
-    setShowModal(!showModal);
-  };
+  const handleShowModal = () => setShowModal(!showModal);
 
   return (
-    <React.Fragment>
-      {showModal && (
-        <Modal onClose={handleShowModal}>
-          <DraftEditor />
-        </Modal>
-      )}
+    <>
+      {
+        showModal && (
+          <Modal onClose={handleShowModal}>
+            <DraftEditor />
+          </Modal>
+        )
+      }
       <Sidebar />
+
       <main className='main'>
         <ComponentToPrint ref={targetRef} />
       </main>
+
       <Footer />
+
       <ControlPanel
         targetRef={targetRef}
         onEdit={handleShowModal}
       />
-    </React.Fragment>
+    </>
   );
 };
 
